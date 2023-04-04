0daf238fd62f3d014fc79db8b60a245db03b4b34
Ricardo Mendes <ricardomlealjr@gmail.com>
2023-04-04 20:59:44 -0300

    feature: add redis

---
+++ .infra/terraform/aws/elasticache/security_group.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "redis_security_group" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "TCP"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = var.private_cidr_blocks
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = "${var.product} redis security group"
+    Environment = var.environment
+    Terraformed = true
+  }
+}
+
+
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "redis-subnet-group"
+  subnet_ids = var.redis_subnets
+
+  tags = {
+    Name        = "${var.product} redis subnet group"
+    Environment = var.environment
+    Terraformed = true
+  }
+}
--- .infra/terraform/aws/network/vpc.tf
+++ .infra/terraform/aws/network/vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${var.product} ${var.environment} Vpc"
+    Name        = "${var.product} ${var.environment} Vpc"
     Environment = var.environment
     Terraformed = true
   }
