d68b8372ae4225a80a78967a8d7a26c9b1fa66df
Clara Melo <mcmelodecarvalho@gmail.com>
2023-04-16 16:20:11 -0400

    --wip-- [skip ci]

--- .infra/terraform/aws/ecr/repository.tf
+++ .infra/terraform/aws/ecr/repository.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "repository" {
-  name                 = "${var.product}${var.environment}"
+  name                 = "${var.product}-${var.environment}"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
--- .infra/terraform/aws/network/vpc_endpoints.tf
+++ .infra/terraform/aws/network/vpc_endpoints.tf
@@ -1,31 +1,35 @@
+locals {
+  subnets_ids = [aws_subnet.subnet_AZ_A_private.id, aws_subnet.subnet_AZ_B_private.id]
+}
+
 resource "aws_vpc_endpoint" "ecr-dkr-endpoint" {
-  vpc_id       = var.vpc_id
+  vpc_id       = aws_vpc.vpc.id
   private_dns_enabled = true
   service_name = "com.amazonaws.us-east-1.ecr.dkr"
   vpc_endpoint_type = "Interface"
-  subnet_ids = var.subnet_ids
+  subnet_ids = locals.subnet_ids
 }
 
 resource "aws_vpc_endpoint" "ecr-api-endpoint" {
-  vpc_id       = var.vpc_id
+  vpc_id       = aws_vpc.vpc.id
   service_name = "com.amazonaws.us-east-1.ecr.api"
   vpc_endpoint_type = "Interface"
   private_dns_enabled = true
-  subnet_ids = var.subnet_ids
+  subnet_ids = locals.subnet_ids
 }
 
 resource "aws_vpc_endpoint" "ecs-agent" {
-  vpc_id       = var.vpc_id
+  vpc_id       = aws_vpc.vpc.id
   service_name = "com.amazonaws.us-east-1.ecs-agent"
   vpc_endpoint_type = "Interface"
   private_dns_enabled = true
-  subnet_ids = var.subnet_ids
+  subnet_ids = locals.subnet_ids
 }
 
 resource "aws_vpc_endpoint" "ecs-telemetry" {
-  vpc_id       = var.vpc_id
+  vpc_id       = aws_vpc.vpc.id
   service_name = "com.amazonaws.us-east-1.ecs-telemetry"
   vpc_endpoint_type = "Interface"
   private_dns_enabled = true
-  subnet_ids = var.subnet_ids
+  subnet_ids = locals.subnet_ids
 }
