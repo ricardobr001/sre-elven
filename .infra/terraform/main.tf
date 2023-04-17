a3cd4181711c7bffaaa05ca3837ea25ee95647b3
Ricardo Mendes <ricardomlealjr@gmail.com>
2023-04-16 22:19:39 -0300

    feat: add ecs

--- .infra/terraform/main.tf
+++ .infra/terraform/main.tf
@@ -24,7 +24,7 @@ data "external" "my_ip" {
 locals {
   environment    = terraform.workspace
   product        = "gobarber"
-  cluster_name = "gobarber-eks-cluster"
+  cluster_name   = "gobarber-eks-cluster"
   vpc_cidr_block = "10.30.0.0/16"
 
   public_az_a_subnet_cidr_block  = "10.30.0.0/24"
@@ -33,103 +33,92 @@ locals {
   private_az_b_subnet_cidr_block = "10.30.3.0/24"
 }
 
-# module "network" {
-#   source = "./aws/network"
+module "network" {
+  source = "./aws/network"
 
-#   environment = local.environment
-#   product     = local.product
+  environment = local.environment
+  product     = local.product
 
-#   vpc_cidr_block = local.vpc_cidr_block
+  vpc_cidr_block = local.vpc_cidr_block
 
-#   public_az_a_subnet_cidr_block  = local.public_az_a_subnet_cidr_block
-#   public_az_b_subnet_cidr_block  = local.public_az_b_subnet_cidr_block
-#   private_az_a_subnet_cidr_block = local.private_az_a_subnet_cidr_block
-#   private_az_b_subnet_cidr_block = local.private_az_b_subnet_cidr_block
-# }
+  public_az_a_subnet_cidr_block  = local.public_az_a_subnet_cidr_block
+  public_az_b_subnet_cidr_block  = local.public_az_b_subnet_cidr_block
+  private_az_a_subnet_cidr_block = local.private_az_a_subnet_cidr_block
+  private_az_b_subnet_cidr_block = local.private_az_b_subnet_cidr_block
+}
 
-# module "rds" {
-#   source = "./aws/rds"
+module "elasticache" {
+  source = "./aws/elasticache"
 
-#   my_ip       = format("%s/%s", data.external.my_ip.result["internet_ip"], 32)
-#   environment = local.environment
-#   product     = local.product
+  environment = local.environment
+  product     = local.product
 
-#   vpc_id     = module.network.vpc_id
-#   db_subnets = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
-# }
+  vpc_id              = module.network.vpc_id
+  redis_subnets       = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
+  private_cidr_blocks = [local.private_az_a_subnet_cidr_block, local.private_az_b_subnet_cidr_block]
+}
 
-# module "elasticache" {
-#   source = "./aws/elasticache"
+module "document_db" {
+  source = "./aws/document_db"
 
-#   environment = local.environment
-#   product     = local.product
+  my_ip       = format("%s/%s", data.external.my_ip.result["internet_ip"], 32)
+  environment = local.environment
+  product     = local.product
 
-#   vpc_id              = module.network.vpc_id
-#   redis_subnets       = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
-#   private_cidr_blocks = [local.private_az_a_subnet_cidr_block, local.private_az_b_subnet_cidr_block]
-# }
+  vpc_id     = module.network.vpc_id
+  db_subnets = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
+}
 
-# module "document_db" {
-#   source = "./aws/document_db"
-
-#   my_ip       = format("%s/%s", data.external.my_ip.result["internet_ip"], 32)
-#   environment = local.environment
-#   product     = local.product
-
-#   vpc_id     = module.network.vpc_id
-#   db_subnets = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
-# }
-
-module ecr {
+module "ecr" {
   source = "./aws/ecr"
 
   environment = local.environment
   product     = local.product
 }
 
-# module loadbalancer {
-#   source = "./aws/loadbalancer"
+module "loadbalancer" {
+  source = "./aws/loadbalancer"
 
-#   environment = local.environment
-#   product     = local.product
+  environment = local.environment
+  product     = local.product
 
-#   vpc_id      = module.network.vpc_id
-#   gobarber_loadbalancer_subnets_ids = [module.network.public_az_a_subnet_id, module.network.public_az_b_subnet_id]
-# }
+  vpc_id                            = module.network.vpc_id
+  gobarber_loadbalancer_subnets_ids = [module.network.public_az_a_subnet_id, module.network.public_az_b_subnet_id]
+}
 
-# module ecs {
-#   source = "./aws/ecs"
+module "ecs" {
+  source = "./aws/ecs"
 
-#   environment = local.environment
-#   product     = local.product
+  environment = local.environment
+  product     = local.product
 
-#   ecr_image   = module.ecr.ecr_image_uri
-#   vpc_id      = module.network.vpc_id
-#   loadbalancer_security_group_id = module.loadbalancer.loadbalancer_security_group_id
-#   loadbalancer_target_group_id = module.loadbalancer.loadbalancer_target_group_id
-#   subnet_ids = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
-# }
+  ecr_image                      = module.ecr.ecr_image_uri
+  vpc_id                         = module.network.vpc_id
+  loadbalancer_security_group_id = module.loadbalancer.loadbalancer_security_group_id
+  loadbalancer_target_group_id   = module.loadbalancer.loadbalancer_target_group_id
+  subnet_ids                     = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
+}
 
-# module rds {
-#   source = "./aws/rds"
-  
-#   environment = local.environment
-#   product     = local.product
+module "rds" {
+  source = "./aws/rds"
 
-#   vpc_id         = module.network.vpc_id
-#   db_subnets = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
-# }
+  environment = local.environment
+  product     = local.product
 
-# module "s3" {
-#   source = "./aws/s3"
+  vpc_id     = module.network.vpc_id
+  db_subnets = [module.network.private_az_a_subnet_id, module.network.private_az_b_subnet_id]
+}
 
-#   environment = local.environment
-#   product     = local.product
-# }
+module "s3" {
+  source = "./aws/s3"
 
-# module "cloudfront" {
-#   source = "./aws/cloudfront"
+  environment = local.environment
+  product     = local.product
+}
 
-#   environment = local.environment
-#   product     = local.product
-# }
+module "cloudfront" {
+  source = "./aws/cloudfront"
+
+  environment = local.environment
+  product     = local.product
+}
