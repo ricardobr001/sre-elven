6ab00fe5dc619d6051afbaeed44e2b0296d37840
Antonio Furlaneto <arfurlaneto@gmail.com>
2023-05-03 11:34:09 -0300

    fix: log mongo url

--- backend/src/shared/infra/typeorm/index.ts
+++ backend/src/shared/infra/typeorm/index.ts
@@ -1,9 +1,5 @@
 import { createConnections } from 'typeorm';
 
-console.log({ host: process.env.POSTGRES_HOST })
-console.log({ port: process.env.POSTGRES_PORT })
-console.log({ username: process.env.POSTGRES_USERNAME })
-console.log({ password: process.env.POSTGRES_PASSWORD })
-console.log({ database: process.env.POSTGRES_DATABASE })
+console.log({ host: process.env.MONGO_URL })
 
 createConnections();
