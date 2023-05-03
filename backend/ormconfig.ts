a28bf1b2876735cb6efb77d7b9377e18ef85bb72
Antonio Furlaneto <arfurlaneto@gmail.com>
2023-05-03 11:28:52 -0300

    fix: using mongo url

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -16,13 +16,10 @@ module.exports = [
   {
     name: 'mongo',
     type: 'mongodb',
-    host: process.env.MONGO_HOST,
-    port: process.env.MONGO_PORT,
-    username: process.env.MONGO_USERNAME,
-    password: process.env.MONGO_PASSWORD,
-    database: process.env.MONGO_DATABASE,
+    url: process.env.MONGO_URL,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     entities: ['./src/modules/**/infra/typeorm/schemas/*.ts'],
-    "synchronize": true,
+    synchronize: true,
   },
 ];
