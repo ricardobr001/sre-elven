e03158a5c6e24a49ce6b99333dc340e958ba2eda
Antonio Furlaneto <arfurlaneto@gmail.com>
2023-05-03 10:46:42 -0300

    chore: logs

--- backend/src/shared/infra/typeorm/index.ts
+++ backend/src/shared/infra/typeorm/index.ts
@@ -1,12 +1,9 @@
 import { createConnections } from 'typeorm';
 
+console.log({ host: process.env.POSTGRES_HOST })
+console.log({ port: process.env.POSTGRES_PORT })
+console.log({ username: process.env.POSTGRES_USERNAME })
+console.log({ password: process.env.POSTGRES_PASSWORD })
+console.log({ database: process.env.POSTGRES_DATABASE })
+
 createConnections();
-
-
-console.log({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-})
