07bf7cd4fb43193903358ebce3ca9a8447d097ca
Antonio Furlaneto <arfurlaneto@gmail.com>
2023-05-03 15:22:48 -0300

    fix: fix health check

--- backend/src/modules/users/infra/http/controllers/HealthController.ts
+++ backend/src/modules/users/infra/http/controllers/HealthController.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from 'express';
-import { getConnection } from 'typeorm';
 
 export default class HealthController {
   async health(request: Request, response: Response): Promise<Response> {
-    await getConnection().runMigrations({ transaction: 'all' });
     return response.status(200).send({message: 'ok'});
   }
 }
